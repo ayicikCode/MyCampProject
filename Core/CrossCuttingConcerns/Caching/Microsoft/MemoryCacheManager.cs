cddf3cb78953818f111678b8680c1b0c0d300689
AHMET TALHA ÖZDEMİR <79546731+ahmo57@users.noreply.github.com>
2022-11-30 22:35:13 +0300

    Add files via upload

---
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,66 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection; //!!!!!!!!!!!!!!
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        //Adapter Pattern (var olan bir sistemi kendi koduma uyarlama!)
+        IMemoryCache _memoryCache;
+        public MemoryCacheManager()
+        {
+            _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+        public void Add(string key, object value, int duration)
+        {
+            _memoryCache.Set(key, value, TimeSpan.FromMinutes(duration)); //duration--> cache de kalacak cakti belirler.
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public bool IsAdd(string key) //Bellekte olup olmadığını kontrol etmek için kullandığımız fonksiyondur!!
+        {
+            return _memoryCache.TryGetValue(key, out _); //out _ --> value yu yani geri birşey döndürmek istemediğimizden sadece bellekte olup olmadığını kontrol etmek istediğimiz için
+        }                                                              //bu tekniği kullanırız.
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern) //çalışma anında bellekten silme işlevini görür!
+        {
+            var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memoryCache) as dynamic;
+            List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+            foreach (var cacheItem in cacheEntriesCollection)
+            {
+                ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+                cacheCollectionValues.Add(cacheItemValue);
+            }
+
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _memoryCache.Remove(key);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        //List<Product> GetAll();
+        //void Add(Product product);
+        //void Update(Product product);
+        //void Delete(Product product);
+        //List<Product> GetAllByCategoryId(int categoryId);//ürünleri kategoriye göre filtreleme
+        //örnek olara bir e-ticaret sitesinde filtreleme tuşuna bastığımızda çalışan kod parçacığıdır..!
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
